41a1ff110cdee28e87ceade9f86bf8a883375ee2
Gary Gutman <mrgaryg68@gmail.com>
2012-12-16 19:41:54 -0500

    renamed script

---
+++ Ch2/script1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# A first Python script
+import sys             # load a library module
+print(sys.platform)
+print (2 ** 100)       # raise 2 to a power
+x ="spam!"
+print (x * 8)          # String repetition
+
+raw_input('Press Enter to exit')        # Use with Python 2.6+
+
+# input('Press Enter to exit')        # Use with Python 3.0+
