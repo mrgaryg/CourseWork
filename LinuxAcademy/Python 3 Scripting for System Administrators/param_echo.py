f0fc236678115adda437ce4ce9a370fbc32f4435
mrgaryg <mrgaryg68@gmail.com>
2018-04-26 00:02:43 -0400

    Adding Linux Academy: Python 3 course work and lab work

---
+++ LinuxAcademy/Python 3 Scripting for System Administrators/param_echo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.6
+
+import sys
+
+print(f"Positional Argument {sys.argv[1:]}")
+print(f"First Argument {sys.argv[1]}")
+
---
+++ LinuxAcademy/Python 3 Scripting for System Administrators/timer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.6
+
+# import time
+from time import localtime, mktime, strftime
+
+start_time = localtime()
+print(f"Time started at {strftime('%X', start_time)}")
+
+# Wait for user to stop timer
+input("Press 'Enter' to stop timer ")
+
+stop_time = localtime()
+difference = mktime(stop_time) - mktime(start_time)
+
+print(f"Timer stopped at {strftime('%X', stop_time)} ")
+print(f"Total time: {difference} seconds ")
