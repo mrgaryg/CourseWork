5a95b71ea886768a6936aa0a25b3f98d4729b1ac
Gary Gutman <mrgaryg68@gmail.com>
2018-04-26 22:44:34 -0400

    Revert "removed oreilly coursework material"
    This reverts commit c2c2408ea93236082f5f766f7ca2000d5833bea7.

---
+++ oreilly/Learning_Python/Ch2/script1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# A first Python script
+import sys             # load a library module
+print(sys.platform)
+print (2 ** 100)       # raise 2 to a power
+x ="spam!"
+print (x * 8)          # String repetition
+title = "Hello World"
+# Use with Python 2.6+
+# raw_input('Press Enter to exit')
+
+# Use with Python 3.0+
+# input('Press Enter to exit')
