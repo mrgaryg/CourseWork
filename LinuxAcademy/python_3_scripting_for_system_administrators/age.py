c77bc77c808e37def5afb9d2a83ca174db2f5cdd
Gary Gutman <mrgaryg68@gmail.com>
2018-05-04 00:27:04 -0400

    reverting the changes to renamed folder

--- LinuxAcademy/python_3_scripting_for_system_administrators/age.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3.6
-
-name = input("what is ur name? ")
-birthdate = input("What is your birthdate? ")
-
-age = int(input("How old are you? "))
-
-print(f"{name} was born on {birthdate}")
-print(f"Half of your age is {age / 2}")
--- LinuxAcademy/python_3_scripting_for_system_administrators/param_echo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3.6
-
-import sys
-
-print(f"Positional Argument {sys.argv[1:]}")
-print(f"First Argument {sys.argv[1]}")
-
