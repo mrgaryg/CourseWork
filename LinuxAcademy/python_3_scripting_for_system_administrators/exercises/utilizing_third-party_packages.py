8e664d8e0c1aba223990861bd5d61cac2a08b9f4
Gary Gutman <mrgaryg68@gmail.com>
2018-05-14 01:21:09 -0400

    started working on 3rd party pkgs script

---
+++ LinuxAcademy/python_3_scripting_for_system_administrators/exercises/utilizing_third-party_packages.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+# https://linuxacademy.com/cp/exercises/view/id/715/module/168
+# Make sure that you have the requests package installed. Now, write a script that does the following:
+# Accepts a URL and destination file name from the user calling the script.
+# Utilizes requests to make an HTTP request to the given URL.
+# Has an optional flag to state whether or not the response should be JSON or HTML (HTML by default).
+# Writes the contents of the page out to the destination.
+# Note: You’ll want to use the text attribute to get the HTML.
